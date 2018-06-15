34f85e137489a13562fc407bee0fb88467a03f75
Dario Mratovich <dario_mratovich@hotmail.com>
2018-06-15 12:04:24 +0800

    Tweaked for my needs
    Added more chocolatey packages and removed some bits.

--- New-Machine.ps1
+++ New-Machine.ps1
@@ -25,12 +25,24 @@ if (-not ((Get-PackageSource -Name chocolatey).IsTrusted)) {
 }
 
 @(
-    "google-chrome-x64",
+    "googlechrome",
     "git.install",
-    "visualstudiocode",
+    "putty.install",
     "fiddler4",
-    "slack",
-    "snagit"
+    "microsoft-teams",
+    "nodejs.install",
+    "vlc",
+    "lastpass",
+    "sourcetree",
+    "vscode",
+    "sql-server-management-studio",
+    "linqpad5.install",
+    "teamviewer",
+    "adobereader",
+    "7zip.install",
+    "notepadplusplus.install",
+    "sysinternals",
+    "paint.net"
 ) | % {
     Write-Progress -Activity "Installing $_"
     Install-Package -Name $_ -ProviderName chocolatey
@@ -53,8 +65,11 @@ if ((& git config push.default) -eq $null) {
 Write-Progress -Activity "Setting git aliases"
 git config --global alias.st "status"
 git config --global alias.co "checkout"
+git config --global alias.cob "checkout -b"
+git config --global alias.cm "!git add -A && git commit -m"
 git config --global alias.df "diff"
 git config --global alias.lg "log --graph --oneline --decorate"
+git config --global alias.undo "reset HEAD~1 --mixed"
 
 Write-Progress -Activity "Setting VS Code as the Git editor"
 git config --global core.editor "code --wait"
@@ -63,52 +78,21 @@ Write-Progress -Activity "Installing PoshGit"
 Install-Module posh-git -Scope CurrentUser
 Add-PoshGitToProfile
 
-Write-Progress -Activity "Enabling Office smileys"
-if (Test-Path HKCU:\Software\Microsoft\Office\16.0) {
-    if (-not (Test-Path HKCU:\Software\Microsoft\Office\16.0\Common\Feedback)) {
-        New-Item HKCU:\Software\Microsoft\Office\16.0\Common\Feedback -ItemType Directory
-    }
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Common\Feedback -Name Enabled -Value 1
-}
-else {
-    Write-Warning "Couldn't find a compatible install of Office"
-}
-
-Write-Progress -Activity "Disabling Outlook notifications"
-if (Test-Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences) {
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name ChangePointer -Value 0
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name NewmailDesktopAlerts -Value 0
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name PlaySound -Value 0
-    Set-ItemProperty -Path HKCU:\Software\Microsoft\Office\16.0\Outlook\Preferences -Name ShowEnvelope -Value 0
-}
-else {
-    Write-Warning "Couldn't find a compatible install of Office"
-}
-
-Write-Progress "Hiding desktop icons"
-if ((Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\).HideIcons -ne 1) {
-    Set-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name HideIcons -Value 1
-    Get-Process explorer | Stop-Process
-}
-
 Write-Progress "Enabling PowerShell on Win+X"
 if ((Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\).DontUsePowerShellOnWinX -ne 0) {
     Set-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name DontUsePowerShellOnWinX -Value 0
     Get-Process explorer | Stop-Process
 }
 
-Write-Progress "Making c:\code"
-if (-not (Test-Path c:\code)) {
-    New-Item c:\code -ItemType Directory
+Write-Progress "Making C:\Code"
+if (-not (Test-Path C:\Code)) {
+    New-Item C:\Code -ItemType Directory
 }
 
-Write-Progress "Making c:\temp"
-if (-not (Test-Path c:\temp)) {
-    New-Item c:\temp -ItemType Directory
+Write-Progress "Making C:\Temp"
+if (-not (Test-Path C:\Temp)) {
+    New-Item C:\Temp -ItemType Directory
 }
-
-Write-Progress "Enabling Windows Subsystem for Linux"
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 Write-Progress -Activity "Reloading PS profile"
 . $PROFILE
